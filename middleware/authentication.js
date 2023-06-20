a78cf6f1aa5506fb9488cef4e21302e05f2a8745
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-06-20 16:04:23 +0530

    middleware update

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,10 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 const authentication = (req, res, next) => {
-    const { token } = req.headers;
+    const { token, boardid, taskid } = req.headers;
     if (token) {
-        const {uid} = jwt.decode(token);
+        const { uid } = jwt.decode(token);
         req.body.uid = uid;
+        req.body.boardid = boardid;
+        req.body.taskid = taskid;
         next();
     } else {
         res.status(400).send({ msg: "login required" });
